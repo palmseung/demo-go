8d723a5028233ca09def313a2b02fe59ffbdae10
palmseung <palmseung@gmail.com>
2021-01-10 01:54:20 +0900

    Conditional operator

--- src/calculator2/cal.go
+++ src/calculator2/cal.go
@@ -29,16 +29,28 @@ func main() {
 	line, _ = reader.ReadString('\n')
 	line = strings.TrimSpace(line)
 
-	if line == "+" {
+	switch line {
+	case "+":
 		fmt.Printf("%d + %d = %d", n1, n2, n1+n2)
-	} else if line == "=" {
+	case "-":
 		fmt.Printf("%d - %d = %d", n1, n2, n1-n2)
-	} else if line == "*" {
+	case "*":
 		fmt.Printf("%d * %d = %d", n1, n2, n1-n2)
-	} else if line == "/" {
+	case "/":
 		fmt.Printf("%d / %d = %d", n1, n2, n1-n2)
-	} else {
+	default:
 		fmt.Println("잘못 입력하셨습니다.")
 	}
 
+	// if line == "+" {
+	// 	fmt.Printf("%d + %d = %d", n1, n2, n1+n2)
+	// } else if line == "=" {
+	// 	fmt.Printf("%d - %d = %d", n1, n2, n1-n2)
+	// } else if line == "*" {
+	// 	fmt.Printf("%d * %d = %d", n1, n2, n1-n2)
+	// } else if line == "/" {
+	// 	fmt.Printf("%d / %d = %d", n1, n2, n1-n2)
+	// } else {
+	// 	fmt.Println("잘못 입력하셨습니다.")
+	// }
 }
