9fbd187feabb9904133f736baceaa93bce067d8a
palmseung <palmseung@gmail.com>
2021-01-10 01:06:53 +0900

    Operator

--- src/operator/op.go
+++ src/operator/op.go
@@ -14,4 +14,30 @@ func main() {
 
 	fmt.Println("a << 1 = ", a<<1)
 	fmt.Println("a >> 1 = ", a>>1)
+
+	var c bool
+	c = 3 > 4
+
+	fmt.Println(" 3 > 4", c)
+
+	var d bool
+	d = 3 < 4
+
+	var e bool
+	e = 23 > 2
+
+	fmt.Println("d = ", d)
+	fmt.Println("e = ", e)
+	fmt.Println("result = ", d && e)
+
+	if 3 > 4 {
+		fmt.Println("거짓")
+	}
+	fmt.Println("참")
+
+	h := 3
+	if h == 3 {
+		fmt.Println("참")
+	}
+
 }
