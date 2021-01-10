bf77a4a27ceff6f0d152fcdd54ef26113f0e83c5
palmseung <palmseung@gmail.com>
2021-01-10 21:36:33 +0900

    Recursive call

---
+++ src/fibonacci/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := f(10)
+	fmt.Println(result)
+}
+
+func f(x int) int {
+	if x == 0 {
+		return 1
+	}
+
+	if x == 1 {
+		return 1
+	}
+
+	return f(x-1) + f(x-2)
+}
--- src/function/func.go
+++ src/function/func.go
@@ -2,20 +2,48 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	a, b := func1(2, 3)
+
+// 	fmt.Println(a, b)
+// }
+
+// func func1(x, y int) (int, int) {
+
+// 	func2(x, y)
+// 	return y, x
+// }
+
+// func func2(x, y int) {
+// 	fmt.Printf("x = %d, y = %d", x, y)
+// 	fmt.Println()
+
+// }
+
+// func main() {
+// 	f1(10)
+// }
+
+// func f1(x int) {
+// 	if x == 0 {
+// 		return
+// 	}
+
+// 	fmt.Println(x)
+// 	f1(x - 1)
+// }
+
 func main() {
-	a, b := func1(2, 3)
+	x := sum(10, 0)
 
-	fmt.Println(a, b)
+	fmt.Println(x)
 }
 
-func func1(x, y int) (int, int) {
+func sum(x int, s int) int {
+	if x == 0 {
+		return s
+	}
 
-	func2(x, y)
-	return y, x
-}
-
-func func2(x, y int) {
-	fmt.Printf("x = %d, y = %d", x, y)
-	fmt.Println()
-
+	s += x
+	return sum(x-1, s)
 }
